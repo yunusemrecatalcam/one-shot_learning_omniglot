f254e617ff52640b914bb4dba4d76477fe627aae
yunusemrecatalcam <yunusemrecatalcam@gmail.com>
2018-09-04 14:38:09 +0300

    importing all training set,not şabeşed yet

--- one_shot.py
+++ one_shot.py
@@ -13,11 +13,15 @@ img = Image.open('images_background/Arcadian/character01/0001_02.png')
 arr = np.asarray(img.getdata()).reshape(img.size)
 print(arr)
 
-path = os.path.join("images_background","Arcadian","character01","0001_02.png")
-for sub in path:
-    print(sub)
-print(path)
-walk = os.walk(path)
+i=0
+
+arc = os.path.join("images_background")
+for alphabet in os.listdir(arc):
+    for characters in os.listdir(os.path.join(arc,alphabet)):
+        for sub in os.listdir(os.path.join(arc,alphabet,characters)):
+            print(os.path.join(arc,alphabet,characters,sub))
+            i +=1
+print(i)
 print(type(walk))
 plt.imshow(arr)
 plt.show()
