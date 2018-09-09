432fe0c242c880d93634d9ad05b5b30710aec348
yunusemrecatalcam <yunusemrecatalcam@gmail.com>
2018-09-09 15:09:58 +0300

    data labeling ok

--- one_shot.py
+++ one_shot.py
@@ -9,19 +9,36 @@ from tensorflow.python import keras
 from keras.callbacks import ModelCheckpoint
 import matplotlib.pyplot as plt
 
-img = Image.open('images_background/Arcadian/character01/0001_02.png')
-arr = np.asarray(img.getdata()).reshape(img.size)
-print(arr)
+
+path = 'images_background/Arcadian/character01/0001_02.png'
+label_idx = path.find('.png') -7
+print(label_idx)
+print(path[label_idx:label_idx+4])
 
 i=0
+train_x = np.zeros((19280,105,105))
+print(train_x.shape)
+train_y = np.zeros((19280))
+
+print(train_x.shape)
 
 arc = os.path.join("images_background")
 for alphabet in os.listdir(arc):
     for characters in os.listdir(os.path.join(arc,alphabet)):
         for sub in os.listdir(os.path.join(arc,alphabet,characters)):
-            print(os.path.join(arc,alphabet,characters,sub))
+            path = os.path.join(arc,alphabet,characters,sub)
+            img = Image.open(path)
+            train_x[i,:,:] = np.asarray(img.getdata()).reshape(img.size)
+            train_y[i] = int(path[path.find('.png')-7:path.find('.png')-3])
+            print(train_y[i])
             i +=1
-print(i)
-print(type(walk))
-plt.imshow(arr)
-plt.show()
+
+plt.imshow(train_x[np.where(train_y==80)][10].reshape(105,105))
+print(path)
+print(int(path[path.find('.png')-7:path.find('.png')-3]))
+
+
+x =10
+a =1
+print(train_y[0:20])
+plt.imshow(train_x[10*a:10*a+x,:,:].reshape(105*x,105))
